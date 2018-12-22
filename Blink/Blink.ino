2e2bd822c53ebda017096c907c392ef860b4d113
sbkindt <stijn.kindt@ugent.be>
2018-12-22 21:01:02 +0100

    added first test for blinking leds with arduino nano

---
+++ Blink/Blink.ino
@@ -0,0 +1,40 @@
+const int BLINK_PIN_1 =   12;
+const int BLINK_PIN_2 =   11;
+const int BLINK_PIN_3 =   10;
+const int BUTTON_PIN  =    2;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(BLINK_PIN_1, OUTPUT);
+  pinMode(BLINK_PIN_2, OUTPUT);
+  pinMode(BLINK_PIN_3, OUTPUT);
+  pinMode(BUTTON_PIN , INPUT );
+}
+
+void loop() {
+
+  if(digitalRead(BUTTON_PIN) == HIGH){
+    digitalWrite(BLINK_PIN_1, HIGH); 
+    delay(333);
+    digitalWrite(BLINK_PIN_2, HIGH); 
+    delay(333);
+    digitalWrite(BLINK_PIN_3, HIGH); 
+    delay(333);
+    digitalWrite(BLINK_PIN_1, LOW);
+    delay(333);
+    digitalWrite(BLINK_PIN_2, LOW);
+    delay(333);
+    digitalWrite(BLINK_PIN_3, LOW);
+    delay(333);
+  }else{
+    digitalWrite(BLINK_PIN_1, LOW);
+    digitalWrite(BLINK_PIN_2, LOW);
+    digitalWrite(BLINK_PIN_3, LOW);
+    delay(10);
+  }
+  
+}
+
+
+
+
